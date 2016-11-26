527d650fc383bff5f66e7538ecbc522128473ca4
justin soderberg <sodejm@gmail.com>
2016-11-26 13:30:48 -0800

    Initial Commit

---
+++ ex3.py
@@ -0,0 +1,24 @@
+print "I will now count my chickens:"
+
+#PEMDAS
+print "Hens", 25 + 30 / 6 # divide 30 by 6 then add 25
+print "Roosters", float(100 - 25 * 3 % 4) # multiply 25 by 3 then get the modulus and subtract from 100
+
+print "Now I will count the eggs:"
+
+print float(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print "Is it true that 3 + 2 < 5 - 7?" #no
+
+print 3 + 2 < 5 - 7 #this will equate to false
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2 #True
+print "Is it greater or equal?", 5 >= -2 #True
+print "Is it less or equal?", 5 <= -2 #False
